21831976e9bcf2c1a17e65457f99de3f5deb40f0
Chris S <christopherrsoto@gmail.com>
2023-05-20 19:15:24 -0700

    remove placeholder

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,6 +5,4 @@ import { Component } from '@angular/core';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'scratch2';
-}
+export class AppComponent { }
