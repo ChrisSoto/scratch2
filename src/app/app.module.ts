54fa0059461a7cee6b0c7c6057f43497f4031a3a
Chris S <christopherrsoto@gmail.com>
2023-05-22 19:43:07 -0700

    added firebase

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,17 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { CommonToolbarComponent } from './common-use/common-toolbar/common-toolbar.component';
+import { initializeApp, provideFirebaseApp } from '@angular/fire/app';
+import { getFirestore, provideFirestore } from '@angular/fire/firestore';
+import { firebaseConfig } from 'firebase_config';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
+    provideFirebaseApp(() => initializeApp(firebaseConfig)),
+    provideFirestore(() => getFirestore()),
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
