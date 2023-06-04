7ce42a7f0eeb1f6741d9cf30bd9e38787dde0e98
Chris S <christopherrsoto@gmail.com>
2023-06-03 17:22:51 -0700

    added meta model

---
+++ src/app/shared/interface/meta.model.ts
@@ -0,0 +1,9 @@
+import { Timestamp } from '@angular/fire/firestore';
+
+export interface Meta {
+    id?: string;
+    created?: Timestamp;
+    createdBy?: string;
+    updated?: Timestamp;
+    updatedBy?: string;
+}
