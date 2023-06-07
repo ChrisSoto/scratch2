68571fcf8d5e1851c35fcade046982afbb960676
Chris S <christopherrsoto@gmail.com>
2023-06-06 20:53:33 -0700

    responsive square directive

--- src/app/apps/church-slide-sync/components/church-slideshow-view/church-slideshow-view.component.ts
+++ src/app/apps/church-slide-sync/components/church-slideshow-view/church-slideshow-view.component.ts
@@ -1,11 +1,15 @@
 import { Component, inject } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ActiveChurchSlideshowService } from '../../services/active-church-slideshow.service';
+import { MakeSquareDirective } from '../../directives/make-square.directive';
 
 @Component({
   selector: 'church-slides-view',
   standalone: true,
-  imports: [CommonModule],
+  imports: [
+    CommonModule,
+    MakeSquareDirective
+  ],
   templateUrl: './church-slideshow-view.component.html',
   styleUrls: ['./church-slideshow-view.component.scss']
 })
---
+++ src/app/apps/church-slide-sync/directives/make-square.directive.ts
@@ -0,0 +1,40 @@
+import { AfterViewChecked, Directive, ElementRef, OnInit, Renderer2, inject } from '@angular/core';
+import { debounceTime, fromEvent, throttleTime } from 'rxjs';
+
+@Directive({
+  selector: '[makeSquare]',
+  standalone: true
+})
+export class MakeSquareDirective implements OnInit, AfterViewChecked {
+
+  private renderer = inject(Renderer2);
+  private el = inject(ElementRef);
+  private domElement = this.el.nativeElement as HTMLElement;
+
+  ngOnInit(): void {
+    fromEvent(window, 'resize')
+      .pipe(
+        throttleTime(500),
+        debounceTime(550),
+      )
+      .subscribe(_ => {
+        this.makeSquare();
+      });
+  }
+
+  private makeSquare() {
+    const parent = this.domElement.parentElement;
+    if (!parent) return;
+    const width = parent.offsetWidth;
+    const height = parent.offsetHeight;
+    // take smaller
+    const smaller = width > height ? height : width;
+    this.renderer.setStyle(this.domElement, 'width', `${smaller * 0.9}px`);
+    this.renderer.setStyle(this.domElement, 'height', `${smaller * 0.9}px`);
+  }
+
+  ngAfterViewChecked(): void {
+    this.makeSquare();
+  }
+
+}
